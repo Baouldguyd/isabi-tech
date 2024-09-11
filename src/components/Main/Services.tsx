fd2dac442d70843b2d03c6cb3fc43400cfb04e94
rahmat afolaranmi <ranmigold01@gmail.com>
2024-09-11 16:37:00 +0100

    mobile

--- src/components/Main/Services.tsx
+++ src/components/Main/Services.tsx
@@ -1,12 +1,20 @@
 import ArrowUpDownIcon from "@/assets/Icon/ArrowUpDownIcon";
 import BackEndIcon from "@/assets/Icon/BackEndIcon";
 import CodeIcon from "@/assets/Icon/CodeIcon";
+import CollateralIcon from "@/assets/Icon/CollateralIcon";
 import CustomIcon from "@/assets/Icon/CustomIcon";
+import DeployIcon from "@/assets/Icon/DeployIcon";
+import GuidelineIcon from "@/assets/Icon/GuidelineIcon";
+import IdentityIcon from "@/assets/Icon/IdentityIcon";
 import CursorBlueIcon from "@/assets/Icon/MouseBlueIcon";
 import MouseLineIcon from "@/assets/Icon/MouseLineIcon";
 import MouseRoundIcon from "@/assets/Icon/MouseRoundIcon";
+import NativeIcon from "@/assets/Icon/NativeIcon";
+import ReactNativeIcon from "@/assets/Icon/ReactNativeIcon";
 import RespIcon from "@/assets/Icon/RespIcon";
+import StrategyIcon from "@/assets/Icon/StrategyIcon";
 import TypoIcon from "@/assets/Icon/TypoIcon";
+import UiUxIcon from "@/assets/Icon/UiUxIcon";
 import VisualIcon from "@/assets/Icon/VisualIcon";
 
 const Services = () => {
@@ -78,8 +86,65 @@ const Services = () => {
     },
   ];
 
+  const appDevelopment = [
+    {
+      text: "Native iOS and Andriod App Development",
+      icon: <NativeIcon />,
+    },
+    {
+      text: "Cross-Platform Development (React Native, Flutter)",
+      icon: <ReactNativeIcon />,
+    },
+    {
+      text: "App Prototyping and UI/UX Design Integration",
+      icon: <UiUxIcon />,
+    },
+    {
+      text: "App Testing, Deployment, and Maintenance",
+      icon: <DeployIcon />,
+    },
+  ];
+
+  const businessPromotion = [
+    {
+      text: "Native iOS and Andriod App Development",
+      icon: <NativeIcon />,
+    },
+    {
+      text: "Cross-Platform Development (React Native, Flutter)",
+      icon: <ReactNativeIcon />,
+    },
+    {
+      text: "App Prototyping and UI/UX Design Integration",
+      icon: <UiUxIcon />,
+    },
+    {
+      text: "App Testing, Deployment, and Maintenance",
+      icon: <DeployIcon />,
+    },
+  ];
+
+  const brandDesign = [
+    {
+      text: "Logo Design and Visual Identity Development",
+      icon: <IdentityIcon />,
+    },
+    {
+      text: "Brand Strategy and Positioning",
+      icon: <StrategyIcon />,
+    },
+    {
+      text: "Brand Guidelines and Style Guides",
+      icon: <GuidelineIcon />,
+    },
+    {
+      text: " Marketing Collateral Design (Business Cards, etc.)",
+      icon: <CollateralIcon />,
+    },
+  ];
+
   return (
-    <div className=" h-[80rem] bg-[#EBEDEE] rounded-md font-[manrope-regular] w-[90%] m-auto">
+    <div className=" h-[150rem] bg-[#EBEDEE] rounded-md font-[manrope-regular] w-[90%] m-auto ">
       {/* Our services */}
       <div className=" h-[10rem]  p-7 flex flex-col gap-2">
         <h1 className="  font-[manrope-extraBold] text-center text-[30px]  font-bold  ">
@@ -106,14 +171,123 @@ const Services = () => {
           </p>
           {/* Our Services */}
           <div className=" mt-6 gap-12 flex flex-col">
-          <div className=" h-[2.5rem] w-[12rem] rounded-md p-2 bg-[#F5F8FA]">
+            <div className=" h-[2.5rem] w-[12rem] rounded-md p-2 bg-[#F5F8FA]">
+              <p className=" font-[manrope-semibold]">Our services include:</p>
+            </div>
+            {/* User Experience */}
+            <span className=" flex flex-col  gap-8">
+              <p>User Experience (UX) Design</p>
+              <div className=" flex gap-4 justify-between">
+                {userExp.map((item, index) => (
+                  <div
+                    key={index}
+                    className=" w-[256px] h-[160px] p-4 flex gap-4 flex-col bg-[#F5F8FA] rounded-md"
+                  >
+                    {/* Icon div */}
+                    <div className=" w-[40px] h-[40px] bg-[#EBEDEE] p-2 rounded-md  flex items-center justify-center border-2">
+                      {item.icon}
+                    </div>
+                    {/* Text div */}
+                    <div>
+                      <p className=" font-[manrope-regular]">{item.text}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </span>
+            {/* User Interface */}
+            <span className=" flex flex-col gap-8">
+              <p>User Interface (UI) Design</p>
+              <div className=" flex gap-4 justify-between">
+                {userInter.map((item, index) => (
+                  <div
+                    key={index}
+                    className=" w-[256px] h-[160px]  rounded-md bg-[#F5F8FA] flex p-4 gap-4 flex-col"
+                  >
+                    {/* icon */}
+                    <div className=" w-[40px] h-[40px] rounded-md bg-[#EBEDEE]  flex justify-center items-center border-2">
+                      {item.icon}
+                    </div>
+                    {/* text */}
+                    <div>
+                      <p>{item.text}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </span>
+
+            {/* Development */}
+
+            <span className=" flex flex-col gap-8">
+              <p> Development</p>
+              <div className=" flex gap-[10rem] justify-start">
+                {development.map((item, index) => (
+                  <div
+                    key={index}
+                    className=" w-[256px] h-[160px]  rounded-md bg-[#F5F8FA] flex p-4 gap-4 flex-col"
+                  >
+                    {/* icon */}
+                    <div className=" w-[40px] h-[40px] rounded-md bg-[#EBEDEE]  flex justify-center items-center border-2">
+                      {item.icon}
+                    </div>
+                    {/* text */}
+                    <div>
+                      <p>{item.text}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </span>
+          </div>
+        </div>
+
+        {/* Mobile App Development */}
+        <div className=" flex flex-col gap-4 ">
+          <p className=" font-[manrope-bold] md:text-2xl mt-6 ">
+            Mobile App Development
+          </p>
+          <p className=" font-[manrope-light] ">
+            We are exceptional in building intuitive and robust mobile apps that
+            enhance user experience and engagement.
+          </p>
+
+          <div className=" h-[2.5rem] w-[12rem] rounded-md p-2 bg-[#F5F8FA] mt-10">
             <p className=" font-[manrope-semibold]">Our services include:</p>
           </div>
-          {/* User Experience */}
-          <span className=" flex flex-col  gap-8">
-            <p>User Experience (UX) Design</p>
+          <div className=" flex gap-4 justify-between">
+            {appDevelopment.map((item, index) => (
+              <div
+                key={index}
+                className=" w-[256px] h-[160px] p-4 flex gap-4 flex-col bg-[#F5F8FA] rounded-md"
+              >
+                {/* Icon div */}
+                <div className=" w-[40px] h-[40px] bg-[#EBEDEE] p-2 rounded-md  flex items-center justify-center border-2">
+                  {item.icon}
+                </div>
+                {/* Text div */}
+                <div>
+                  <p className=" font-[manrope-regular]">{item.text}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          {/* Business Promotion */}
+          <div className=" flex flex-col gap-4 ">
+            <p className=" font-[manrope-bold] md:text-2xl mt-6 ">
+              Business Promotion
+            </p>
+            <p className=" font-[manrope-light] ">
+              At iSabiTech, we help you maximize visibility & growth through
+              strategic marketing solutions.
+            </p>
+            <div className=" h-[2.5rem] w-[12rem] rounded-md p-2 bg-[#F5F8FA] mt-10">
+              <p className=" font-[manrope-semibold]">Our services include:</p>
+            </div>
+
             <div className=" flex gap-4 justify-between">
-              {userExp.map((item, index) => (
+              {businessPromotion.map((item, index) => (
                 <div
                   key={index}
                   className=" w-[256px] h-[160px] p-4 flex gap-4 flex-col bg-[#F5F8FA] rounded-md"
@@ -129,52 +303,37 @@ const Services = () => {
                 </div>
               ))}
             </div>
-          </span>
-          {/* User Interface */}
-          <span className=" flex flex-col gap-8">
-            <p>User Interface (UI) Design</p>
+
+              {/* Brand Design */}
+              <div  className=" flex flex-col gap-4 ">
+              <p className=" font-[manrope-bold] md:text-2xl mt-6 ">
+              Brand Design
+            </p>
+            <p className=" font-[manrope-light] ">
+            We help you define your business identity with unique & impactful branding designs..
+            </p>
+            <div className=" h-[2.5rem] w-[12rem] rounded-md p-2 bg-[#F5F8FA] mt-10">
+              <p className=" font-[manrope-semibold]">Our services include:</p>
+            </div>
+            <div>Branding and Identity</div>
             <div className=" flex gap-4 justify-between">
-              {userInter.map((item, index) => (
+              {brandDesign.map((item, index) => (
                 <div
                   key={index}
-                  className=" w-[256px] h-[160px]  rounded-md bg-[#F5F8FA] flex p-4 gap-4 flex-col"
+                  className=" w-[256px] h-[160px] p-4 flex gap-4 flex-col bg-[#F5F8FA] rounded-md"
                 >
-                  {/* icon */}
-                  <div className=" w-[40px] h-[40px] rounded-md bg-[#EBEDEE]  flex justify-center items-center border-2">
+                  {/* Icon div */}
+                  <div className=" w-[40px] h-[40px] bg-[#EBEDEE] p-2 rounded-md  flex items-center justify-center border-2">
                     {item.icon}
                   </div>
-                  {/* text */}
+                  {/* Text div */}
                   <div>
-                    <p>{item.text}</p>
+                    <p className=" font-[manrope-regular]">{item.text}</p>
                   </div>
                 </div>
               ))}
             </div>
-          </span>
-
-          {/* Development */}
-
-          <span className=" flex flex-col gap-8">
-            <p> Development</p>
-            <div className=" flex gap-[10rem] justify-start">
-              {development.map((item, index) => (
-                <div
-                  key={index}
-                  className=" w-[256px] h-[160px]  rounded-md bg-[#F5F8FA] flex p-4 gap-4 flex-col"
-                >
-                  {/* icon */}
-                  <div className=" w-[40px] h-[40px] rounded-md bg-[#EBEDEE]  flex justify-center items-center border-2">
-                    {item.icon}
-                  </div>
-                  {/* text */}
-                  <div>
-                    <p>{item.text}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </span>
-
+              </div>
           </div>
         </div>
       </div>
